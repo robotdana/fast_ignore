d7509d8d67b4d10edcc47522f9e2f40db40d3ae4
Dana Sherson <robot@dana.sh>
2020-11-07 16:24:33 +1100

    Temporarily disable rule compressing for simplicity

--- lib/fast_ignore/rule.rb
+++ lib/fast_ignore/rule.rb
@@ -6,40 +6,40 @@ class FastIgnore
     alias_method :negation?, :negation
     undef :negation
 
-    attr_reader :component_rules
-    attr_reader :component_rules_count
+    # attr_reader :component_rules
+    # attr_reader :component_rules_count
 
     attr_reader :dir_only
     alias_method :dir_only?, :dir_only
     undef :dir_only
 
-    attr_reader :squashable_type
+    # attr_reader :squashable_type
     attr_reader :rule
 
-    def squash(rules)
-      # component rules is to improve the performance of repos with many .gitignore files. e.g. linux.
-      component_rules = rules.flat_map(&:component_rules)
-      ::FastIgnore::Rule.new(
-        ::Regexp.union(component_rules.map(&:rule)).freeze,
-        @negation, @anchored, @dir_only, component_rules
-      )
-    end
+    # def squash(rules)
+    #   # component rules is to improve the performance of repos with many .gitignore files. e.g. linux.
+    #   component_rules = rules.flat_map(&:component_rules)
+    #   ::FastIgnore::Rule.new(
+    #     ::Regexp.union(component_rules.map(&:rule)).freeze,
+    #     @negation, @anchored, @dir_only, component_rules
+    #   )
+    # end
 
-    def initialize(rule, negation, anchored, dir_only, component_rules = self) # rubocop:disable Metrics/MethodLength
+    def initialize(rule, negation, anchored, dir_only, _component_rules = self)
       @rule = rule
       @anchored = anchored
       @dir_only = dir_only
       @negation = negation
-      @component_rules = component_rules
-      @component_rules_count = component_rules == self ? 1 : component_rules.length
+      # @component_rules = component_rules
+      # @component_rules_count = component_rules == self ? 1 : component_rules.length
 
-      @squashable_type = if anchored && negation
-        1
-      elsif anchored
-        0
-      else
-        ::Float::NAN # because it doesn't equal itself
-      end
+      # @squashable_type = if anchored && negation
+      #   1
+      # elsif anchored
+      #   0
+      # else
+      #   ::Float::NAN # because it doesn't equal itself
+      # end
 
       freeze
     end
